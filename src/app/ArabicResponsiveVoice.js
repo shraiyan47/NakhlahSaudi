9b29201b117dfea510f33b1abfda8141e4495da1
shahadat.hossain <shahadat.hossain@leads-bd.com>
2024-02-18 12:54:19 +0600

    v0.0.10

--- src/app/ArabicResponsiveVoice.js
+++ src/app/ArabicResponsiveVoice.js
@@ -1,42 +1,42 @@
-import React, { useState } from 'react';
-// import responsiveVoice from 'responsivevoice';
-// import ResponsiveVoice from 'responsivevoice';
-var responsiveVoice = require('responsiveVoice');
+// import React, { useState } from 'react';
+// // import responsiveVoice from 'responsivevoice';
+// // import ResponsiveVoice from 'responsivevoice';
+// var responsiveVoice = require('responsiveVoice');
 
-const ArabicSpeechResponsiveVoice = (param) => {
-    const [isSpeaking, setIsSpeaking] = useState(false);
+// const ArabicSpeechResponsiveVoice = (param) => {
+//     const [isSpeaking, setIsSpeaking] = useState(false);
 
     
-    const speakArabic = () => {
-        console.log("Arabic bunga bunga....",param.audioData)
+//     const speakArabic = () => {
+//         console.log("Arabic bunga bunga....",param.audioData)
         
-        const options = {
-            lang: 'US English Female', // Specify Arabic language
-            pitch: 1, // Adjust pitch as needed
-            rate: 1, // Adjust speech rate as needed
-            volume: 50, // Adjust volume as needed
-        };
+//         const options = {
+//             lang: 'US English Female', // Specify Arabic language
+//             pitch: 1, // Adjust pitch as needed
+//             rate: 1, // Adjust speech rate as needed
+//             volume: 50, // Adjust volume as needed
+//         };
         
-        console.log("ResponsiveVoice --> ",responsiveVoice)
+//         console.log("ResponsiveVoice --> ",responsiveVoice)
       
-        try {
-            setIsSpeaking(true);
-            console.log("responsiveVoice --> ",responsiveVoice.speak)
-            responsiveVoice.speak("Boom Boom", options);
-        } catch (err) {
-            console.error('Error speaking:', err);
-        } finally {
-            setIsSpeaking(false);
-        }
-    };
+//         try {
+//             setIsSpeaking(true);
+//             console.log("responsiveVoice --> ",responsiveVoice.speak)
+//             responsiveVoice.speak("Boom Boom", options);
+//         } catch (err) {
+//             console.error('Error speaking:', err);
+//         } finally {
+//             setIsSpeaking(false);
+//         }
+//     };
 
-    return (
-        <div>
-            <button onClick={speakArabic} disabled={isSpeaking} type="button">
-                {isSpeaking ? 'Stop Speaking' : 'Speak Arabic'}
-            </button>
-        </div>
-    );
-};
+//     return (
+//         <div>
+//             <button onClick={speakArabic} disabled={isSpeaking} type="button">
+//                 {isSpeaking ? 'Stop Speaking' : 'Speak Arabic'}
+//             </button>
+//         </div>
+//     );
+// };
 
-export default ArabicSpeechResponsiveVoice;
+// export default ArabicSpeechResponsiveVoice;
